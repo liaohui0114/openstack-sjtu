37f7141d95d58afe86759e05ae732d58e2044050
liaohui0114 <liaohui0114@163.com>
2015-11-26 19:49:24 +0800

    bugs fixed

--- HorizonDemo/hybrid_cloud/api/User.py
+++ HorizonDemo/hybrid_cloud/api/User.py
@@ -5,14 +5,13 @@ USER = "admin"
 PASSWD = "1"
 TENANT="demo"
 
-
+##to get nova credentials
 def get_nova_credentials(url=AUTHURL):
     d = {}
     d['username'] = USER
     d['api_key'] = PASSWD
     d['auth_url'] = AUTHURL
     d['project_id'] = TENANT
-    
     d['auth_url'] = url
     return d
 
